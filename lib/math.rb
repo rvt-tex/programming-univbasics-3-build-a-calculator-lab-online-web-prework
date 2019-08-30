8cbc36e0241c092e09f36712ad4c5a7bea1e70a5
Ruel Tingling <rvtingling@yahoo.com>
2019-08-30 14:19:43 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,40 @@
 def addition(num1, num2)
+  num1 = 5
+  num2 = 4
+  addition = ("num1" :+ "num2")
+  puts "#{addition}"
+end
 
 end
 
 def subtraction(num1, num2)
-
+  num1 = 10
+  num2 = 5
+  subtraction = ("num1" :- "num2")
+  puts "#{subtraction}"
 end
 
 def division(num1, num2)
-
+  num1 = 50
+  num2 = 2
+  division = ("num1" :/ "num2")
+  puts "#{division}"
 end
 
 def multiplication(num1, num2)
-
+  num1 = 4
+  num2 = 30
+  multiplication = ("num1" :* "num2")
+  puts "#{multiplication}"
 end
 
 def modulo(num1, num2)
-
+  num1 = 34
+  num2 = 5
+  modulo = ("num1" :% "num2")
+  puts "#{modulo}"
 end
 
 def square_root(num)
-
+  
 end
